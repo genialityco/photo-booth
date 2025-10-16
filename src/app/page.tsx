98d554bb09cf01068e423ce2060012af334b965a
balderban <maria.perdomo@mocion.com.co>
2025-10-16 15:45:28 -0500

    agregando boton de habilitar camara

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,46 +2,55 @@
 "use client";
 
 // import { useRouter } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import ButtonPrimary from "@/app/items/ButtonPrimary";
 import Page from "./home/page";
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [enabledCamara, setEnabledCamara] = useState(false);
 
-  useEffect(() => {
-    const initCamera = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: { facingMode: "user", width: { ideal: 320 }, height: { ideal: 240 } },
-        });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-      } catch (err) {
-        console.error("Error al acceder a la cámara:", err);
+  const initCamera = async () => {
+    try {
+      setEnabledCamara(true);
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: "user", width: { ideal: 320 }, height: { ideal: 240 } },
+      });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
       }
-    };
+    } catch (err) {
+      console.error("Error al acceder a la cámara:", err);
+    }
+  };
 
-    initCamera();
-
-    return () => {
-      if (videoRef.current?.srcObject) {
-        (videoRef.current.srcObject as MediaStream)
-          .getTracks()
-          .forEach((track) => track.stop());
-      }
-    };
-  }, []);
+  // return () => {
+  //   // if (videoRef.current?.srcObject) {
+  //   //   (videoRef.current.srcObject as MediaStream)
+  //   //     .getTracks()
+  //   //     .forEach((track) => track.stop());
+  //   // }
+  // };
 
   return (
     <>
-    <div className="antialiased min-h-screen relative">
-
-     <div
+      <div className="antialiased min-h-screen relative">
+        <div
           className="fixed inset-0 -z-10 bg-cover bg-center "
           style={{ backgroundImage: "url('/images/frame.jpg')" }}
         />
-      <Page />
-    </div>
+        {!enabledCamara ? (
+          <ButtonPrimary
+            onClick={initCamera}
+            label={"habilitar camara"}
+            imageSrc="/images/btn_principal.png"
+            width={220}
+            height={68}
+            ariaLabel="habilitar camara"
+          />
+        ) : (
+          <Page />
+        )}
+      </div>
     </>
   );
 }
