422829c4f3743e798879646e8e28b93f3a552864
Sebastian Cardona Rios <112008261+sebastian010800@users.noreply.github.com>
2025-11-05 09:27:59 -0500

    fix: brandKey

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import React from "react";
 import Landing from "./home/components/public/landing";
 import Page from "./home/page";
 
-type BrandKey = "juanvaldez" | "colombina" | "alpina" | "macpollo";
-
+type BrandKey = "col40_alien" | "col40_angel" | "col40_cyborg";
 export default function AppRoot() {
   const [enabledCamara, setEnabledCamara] = React.useState(false);
   const [, setBrand] = React.useState<BrandKey | null>(null);
